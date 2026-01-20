4344b64f4443aaa6b5b057f21f562ad0b98facbf
mu001999 <mu001999@outlook.com>
2026-01-21 01:20:45 +0800

    clean

--- tokio-macros/src/entry.rs
+++ tokio-macros/src/entry.rs
@@ -400,19 +400,18 @@ fn parse_knobs(mut input: ItemFn, is_test: bool, config: FinalConfig) -> TokenSt
         // token, not the whole tokens. We can work around this limitation by
         // using the first/last span of the tokens like
         // `syn::Error::new_spanned` does.
-        let start = last_stmt
-            .next()
-            .map_or_else(Span::call_site, |t| Span::call_site().located_at(t.span()));
-        let end = last_stmt
-            .last()
-            .map_or(start, |t| Span::call_site().located_at(t.span()));
+        let start = last_stmt.next().map_or_else(Span::call_site, |t| t.span());
+        let end = last_stmt.last().map_or(start, |t| t.span());
         (start, end)
     };
 
     let crate_path = config
         .crate_name
         .map(ToTokens::into_token_stream)
-        .unwrap_or_else(|| Ident::new("tokio", last_stmt_start_span).into_token_stream());
+        .unwrap_or_else(|| {
+            let ident = Ident::new("tokio", Span::call_site().located_at(last_stmt_start_span));
+            ident.into_token_stream()
+        });
 
     let mut rt = match config.flavor {
         RuntimeFlavor::CurrentThread | RuntimeFlavor::Local => {
